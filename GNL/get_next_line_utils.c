059b1fa6bbf52c302bdbc1ddb4493946d1089cf8
jham <irang4605@naver.com>
2022-01-18 15:55:38 +0900

    get_next_line

---
+++ GNL/get_next_line_utils.c
@@ -0,0 +1,84 @@
+#include "get_next_line.h"
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char    *tmp;
+    size_t  s_len;
+    size_t  i;
+
+	i = 0;
+    s_len = ft_strlen(s);
+    if (s_len < start)
+        return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	tmp = (char *)malloc(sizeof(char) * (len + 1));
+    if (!tmp) 
+		return (NULL);
+    ft_strlcpy(tmp, s + start, len + 1);
+	return (tmp);
+}     
+
+size_t  ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
+int ft_strchr(const char *s, int c)
+{
+	if (s == 0)
+		return (0);
+	while (*s)
+	{
+		if (*s == (char) c)
+		return (1);
+		s++;
+	}
+	return (0);
+}
+
+size_t  ft_strlcpy(char *dst, const char *src, size_t n)
+{
+    size_t  i;
+    size_t  result;
+
+	i = 0;
+    result = ft_strlen(src);
+    if (!dst || !src)
+		return (0);
+    if (n != 0)
+    {
+		while (src[i] && i < n - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+    }
+    return (result);
+}
+
+char    *ft_strdup(const char *str)
+{
+	char    *tmp;
+	size_t  str_len;
+	size_t  i;
+
+    i = 0;
+	str_len = ft_strlen(str);
+    tmp = (char *)malloc(sizeof(char) * (str_len + 1));
+    if (!tmp)
+        return (NULL);
+    while (str[i]) 
+    {
+        tmp[i] = str[i];
+        i++;
+    }
+    tmp[i] = '\0';
+    return (tmp); 
+}
