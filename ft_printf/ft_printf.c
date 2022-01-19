9124afa037d5439023e203c54ea3fa277cc54410
jham <irang4605@naver.com>
2022-01-19 16:36:59 +0900

    null_error

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,22 +6,40 @@
 /*   By: hamjongseog <hamjongseog@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 21:19:08 by jham              #+#    #+#             */
-/*   Updated: 2022/01/19 13:28:57 by hamjongseog      ###   ########.fr       */
+/*   Updated: 2022/01/19 16:36:34 by hamjongseog      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-int ft_setform(const char **fmt, va_list ap)
+int ft_print_s(va_list ap) //abc를 가리키는 ap 리스트가 들어옴
+{
+	int c;
+	char *str;
+
+	c = 0;
+	str = (char *)va_arg(ap, char *); //char * 크기만큼 값을 가져오고 그 크기만큼 이동, str에 넣음
+	if (!str)
+		return (write(1, "(null)", 6));
+	while (*str) //str abc 쭈욱 끝까지 널까지
+	{
+		write(1, str, 1);
+		str++;
+		c++;
+	}
+	return (c);
+}
+
+int ft_setform(const char **fmt, va_list ap) //s의 주소값 ,  ap 들어옴
 {
 	int c;
 
 	c = 0;
 	if (**fmt == '%')
 		return (write(1, "%", 1));
-	while (**fmt)
+	while (**fmt) //s
 	{
 		if (**fmt == 'c')
 		{
@@ -30,11 +48,15 @@ int ft_setform(const char **fmt, va_list ap)
 			(*fmt)++;
 			return (1);
 		}
+		else if (**fmt == 's') //s라면  여기로들어옴
+		{
+			return (ft_print_s(ap));
+		}
 	}
 	return (0);
 }
 
-int ft_printf(const char *fmt, ...)
+int ft_printf(const char *fmt, ...) //%s , abc
 {
 	va_list ap; //담을 가상 공간을 만들어줌
 	int result; //반환값 저장
@@ -43,8 +65,9 @@ int ft_printf(const char *fmt, ...)
 	if (fmt == NULL)
 		return (-1);
 	result = 0;
-	va_start(ap, fmt);	 //ap에게 가변인자중 첫번째 인자의 주소를 알려줌
-	while (*fmt != '\0') //fmt를 널까지 돌리는데
+	res = 0;
+	va_start(ap, fmt);	 //ap에게 가변인자중 첫번째 인자의 주소를 알려줌 abc의 주소를 알려줌
+	while (*fmt != '\0') //fmt를 널까지 돌리는데 %s
 	{
 		if (*fmt != '%') //만약 %이 아니라면 계속 출력한다.
 		{
@@ -53,8 +76,10 @@ int ft_printf(const char *fmt, ...)
 		}
 		else //%일 경우에 들어와서 해당으로 출력, 문자의 수 반환
 		{
+			fmt++;						// fmt가 s를 가르킴
+			res = ft_setform(&fmt, ap); //일단 가변인수 ap와 %다음 문자를 넘겨준다. s의주소값 , ap를 넘김
+			result += res;
 			fmt++;
-			res = ft_setform(&fmt, ap); //일단 가변인수 ap와%다음 문자를 넘겨준다.
 		}
 	}
 	va_end(ap);
@@ -64,9 +89,13 @@ int ft_printf(const char *fmt, ...)
 int main(void)
 {
 	int res;
-	char a = 'a';
+	int real;
+	char a[10] = "abc";
 
-	res = ft_printf("%c\n", a);
-	printf("len : %d\n", res);
+	res = ft_printf("%c", '\0');
+	printf("%d\n", res);
+
+	real = printf("%c\n", '\0');
+	printf("%d\n", real);
 	return (0);
 }
