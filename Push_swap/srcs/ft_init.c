c6fbee653b02fa5d7b9114b6cac3a1300acbd78d
jham <irang4605@naver.com>
2022-04-11 21:54:50 +0900

    up

--- Push_swap/srcs/ft_init.c
+++ Push_swap/srcs/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: hamjongseog <hamjongseog@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 17:54:14 by hamjongseog       #+#    #+#             */
-/*   Updated: 2022/04/06 17:54:29 by hamjongseog      ###   ########.fr       */
+/*   Updated: 2022/04/11 20:24:31 by hamjongseog      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,44 @@ t_stack *ft_stack_init(void)
     tmp->data = 0;
     tmp->next = NULL;
     return (tmp);
+}
+
+void ft_arr_put(char **split_arr, long long *int_arr, int *jdx, int arr_size)
+{
+    int split_size;
+    int split_idx;
+    long long tmp;
+
+    split_size = ft_split_arrsize(split_arr);
+    if (split_size > 1)
+    {
+        split_idx = 0;
+        while (split_idx < split_size)
+        {
+            tmp = ft_atoll(split_arr[split_idx++]);
+            int_arr[arr_size - 1 - *jdx] = tmp;
+            *jdx += 1;
+        }
+    }
+    else
+    {
+        tmp = ft_atoll(*split_arr);
+        int_arr[arr_size - 1 - *jdx] = tmp;
+        *jdx += 1;
+    }
+}
+
+void ft_init_arr(char **av, long long *int_arr, int ac, int arr_size)
+{
+    int idx;
+    int jdx;
+    char **split_arr;
+
+    idx = 1;
+    jdx = 0;
+    while (idx < ac)
+    {
+        split_arr = ft_split(av[idx], ' ');
+        ft_arr_put(split_arr, int_arr, &jdx, arr_size);
+    }
 }
--- Push_swap/srcs/utility.c
+++ Push_swap/srcs/utility.c
@@ -6,7 +6,7 @@
 /*   By: hamjongseog <hamjongseog@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 17:51:41 by hamjongseog       #+#    #+#             */
-/*   Updated: 2022/04/06 18:56:03 by hamjongseog      ###   ########.fr       */
+/*   Updated: 2022/04/11 21:54:38 by hamjongseog      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,34 @@ int ft_error(int c)
     write(1, "Error\n", 6);
     exit(c);
     return (c);
+}
+
+int ft_isspace(char c)
+{
+    return (c == '\f' || c == '\n' || c == 'r' ||
+            c == '\t' || c == '\v' || c == ' ');
+}
+
+long long ft_atoll(char *str)
+{
+    long long result;
+    int sign;
+
+    result = 0;
+    sign = 1;
+    while (ft_isspace(*str))
+        str++;
+    if (*str == '+' || *str == '-')
+    {
+        if (*str++ == '-')
+            sign *= -1;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    if (ft_isalpha(*str))
+        ft_error(0);
+    return (sign * result);
 }
